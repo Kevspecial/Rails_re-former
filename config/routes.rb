b78436aca57f0f9b81c304e949938464ce3d3999
kelvin <knwokike@gmail.com>
2023-10-06 15:12:32 +0200

    user settings

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # Create a :users resource with only :new and :create actions
+  resources :users, only: [:new, :create]
+
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
